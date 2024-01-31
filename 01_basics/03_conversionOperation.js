896c61a7f8e51fd6055f01254c34814c57633c90
Md Mushfique Raza <114329904+786Mushfique@users.noreply.github.com>
2024-01-31 10:57:11 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,6 +3,9 @@ let score = "hitesh"
 //console.log(typeof score);
 //console.log(typeof(score));
 
+let score = "33abc"
+console.log(typeof score);
+
 let valueInNumber = Number(score)
 //console.log(typeof valueInNumber);
 //console.log(valueInNumber);
